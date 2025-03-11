5d06e846b359d35294f11fba358bca09af647e36
Pei Meng <2398696309@qq.com>
2025-03-11 17:20:56 +0800

    feat: add load_aux_state

--- runtime/include/plane.h
+++ runtime/include/plane.h
@@ -3,4 +3,7 @@
 
 #define MAX_AUX_PLANES        3U
 
+#define PLANE_TO_ARRAY(plane) ((plane) - 1)
+#define ARRAY_TO_PLANE(array) ((array) + 1)
+
 #endif
--- runtime/rsi/plane.c
+++ runtime/rsi/plane.c
@@ -6,6 +6,61 @@
 #include <rsi-handler.h>
 #include <smc-rsi.h>
 
+static void load_aux_sysregs(struct rec *rec, STRUCT_TYPE sysreg_state *sysregs)
+{
+  rec->sysregs.sp_el0 = sysregs->sp_el0;
+  rec->sysregs.sp_el1 = sysregs->sp_el1;
+  rec->sysregs.elr_el1 = sysregs->elr_el1;
+  rec->sysregs.spsr_el1 = sysregs->spsr_el1;
+  rec->sysregs.pmcr_el0 = sysregs->pmcr_el0;
+  rec->sysregs.tpidrro_el0 = sysregs->tpidrro_el0;
+  rec->sysregs.tpidr_el0 = sysregs->tpidr_el0;
+  rec->sysregs.csselr_el1 = sysregs->csselr_el1;
+  rec->sysregs.sctlr_el1 = sysregs->sctlr_el1;
+  rec->sysregs.sctlr2_el1 = sysregs->sctlr2_el1;
+  rec->sysregs.actlr_el1 = sysregs->actlr_el1;
+  rec->sysregs.cpacr_el1 = sysregs->cpacr_el1;
+  rec->sysregs.ttbr0_el1 = sysregs->ttbr0_el1;
+  rec->sysregs.ttbr1_el1 = sysregs->ttbr1_el1;
+  rec->sysregs.tcr_el1 = sysregs->tcr_el1;
+  rec->sysregs.esr_el1 = sysregs->esr_el1;
+  rec->sysregs.afsr0_el1 = sysregs->afsr0_el1;
+  rec->sysregs.afsr1_el1 = sysregs->afsr1_el1;
+  rec->sysregs.far_el1 = sysregs->far_el1;
+  rec->sysregs.mair_el1 = sysregs->mair_el1;
+  rec->sysregs.vbar_el1 = sysregs->vbar_el1;
+
+  rec->sysregs.contextidr_el1 = sysregs->contextidr_el1;
+  rec->sysregs.tpidr_el1 = sysregs->tpidr_el1;
+  rec->sysregs.amair_el1 = sysregs->amair_el1;
+  rec->sysregs.cntkctl_el1 = sysregs->cntkctl_el1;
+  rec->sysregs.par_el1 = sysregs->par_el1;
+  rec->sysregs.mdscr_el1 = sysregs->mdscr_el1;
+  rec->sysregs.mdccint_el1 = sysregs->mdccint_el1;
+  rec->sysregs.disr_el1 = sysregs->disr_el1;
+  MPAM(rec->sysregs.mpam0_el1 = sysregs->mpam0_el1;)
+
+  rec->sysregs.cntpoff_el2 = sysregs->cntpoff_el2;
+  rec->sysregs.cntvoff_el2 = sysregs->cntvoff_el2;
+  rec->sysregs.cntp_ctl_el0 = sysregs->cntp_ctl_el0;
+  rec->sysregs.cntp_cval_el0 = sysregs->cntp_cval_el0;
+  rec->sysregs.cntv_ctl_el0 = sysregs->cntv_ctl_el0;
+  rec->sysregs.cntv_cval_el0 = sysregs->cntv_cval_el0;
+}
+
+static void load_aux_state(struct rec *rec, struct rsi_plane_enter *enter, STRUCT_TYPE sysreg_state *sysregs)
+{
+  INFO("Loading aux state\n");
+
+  load_aux_sysregs(rec, sysregs);
+
+  rec->pc = enter->pc;
+
+  for (int i = 0; i < PLANE_EXIT_NR_GPRS; i++) {
+    rec->regs[i] = enter->gprs[i];
+  }
+}
+
 void handle_rsi_plane_enter(struct rec *rec, struct rsi_result *res)
 {
   unsigned int plane_index = rec->regs[1];
@@ -56,7 +111,8 @@ void handle_rsi_plane_enter(struct rec *rec, struct rsi_result *res)
   run = (struct rsi_plane_run *)buffer_granule_map(gr, SLOT_RSI_CALL);
   assert(run != NULL);
 
-  INFO("Aux-plane's index = %d, pc = 0x%lx\n", plane_index, run->enter.pc);
+  /* Switch to aux plane */
+  load_aux_state(rec, &run->enter, &rd->sysregs[PLANE_TO_ARRAY(plane_index)]);
 
   /* Unmap rd granule and PlaneRun granule */
   buffer_unmap(rd);
